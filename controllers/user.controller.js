3f369a91e636aa41b612641942e455d2b912b98a
Gaurav Sharma <gau1711sha@gmail.com>
2025-04-03 14:30:17 +0530

    updated user controller and routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+import { Notification } from '../models/notification.model.js';
 import User from '../models/user.model.js'
 
 export const getUserProfile = async (req,res)=>{
@@ -22,8 +23,26 @@ export const getUserProfile = async (req,res)=>{
 
 }
 
-export const getSuggestedUsers=()=>{
-
+export const getSuggestedUsers=async(req,res)=>{
+try {
+    const userId = req.user._id;
+    const usersFolllowedByMe = await User.findById(userId).select("following");
+    const users = await User.aggregate([
+        {
+            $match:{
+                _id:{$ne:userId}
+            }
+        },
+        {$sample:{size:10}}
+    ])
+    const filteredUsers =  users.filter(user=>!usersFolllowedByMe.following.includes(user._id));
+    const suggestedUsers = filteredUsers.slice(0,4);
+    suggestedUsers.forEach(user=>user.password = null);
+    return res.status(200).json(suggestedUsers);
+} catch (error) {
+    console.log("Error in suggested users",error.message);
+    res.status(500).json({error:error.message})
+}
 }
 
 export const followUnfollowUser = async (req, res) => {
@@ -56,7 +75,18 @@ export const followUnfollowUser = async (req, res) => {
             await User.findByIdAndUpdate(myId, { $push: { following: userId } });
             await User.findByIdAndUpdate(userId, { $push: { followers: myId } });
             // send notification to user
-            
+
+            const newNotification = new Notification({
+                type:"follow",
+                   from:myId,
+                   to:userId
+            });
+
+            await newNotification.save();
+
+           
+
+
             return res.status(200).json({ message: "Followed successfully" });
         }
         
@@ -67,6 +97,55 @@ export const followUnfollowUser = async (req, res) => {
 };
 
 
-export const updateUserProfile = ()=>{
-    
+export const updateUser = async(req,res)=>{
+    const {fullName,email,username,currentPassword,newPassword,bio,link}  =req.body
+let {profileImg, coverImg} = req.body
+    const userId = req.user._id;
+
+   try {
+    const user = await User.findById(userId);
+     if(!user){
+         return res.status(400).json({error:"log in first"})
+     }
+
+     if(currentPassword && newPassword){
+        const isMatch = await bcrypt.compare(currentPassword,user.password);
+        if(!isMatch){
+           res.status(400).json({error:"Invalid password"});
+        }
+        if(newPassword.length<6){
+            return res.status(400).json({error:"password must be atleast 6 character long"});
+        }
+     }
+
+    //  if(profileImg){
+
+    //  }
+    //  if(coverImg){
+
+    //  }
+
+
+    user.fullName = fullName || user.fullName;
+    user.email  =email || user.email;
+    user.username = username || user.username;
+    user.bio = bio || user.bio;
+    user.link = link || user.link;
+    user.profileImg = profileImg || user.profileImg;
+    user.coverImg = coverImg || user.coverImg;
+
+
+user =  await user.save();
+user.password =  null;
+    return res.status(200).json(user);
+
+
+
+ 
+   } catch (error) {
+    res.status(500).json({error:"Internal server errro"})
+    console.log("error in updateuser controller")
+   }
+
+
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {followUnfollowUser, getSuggestedUsers, getUserProfile, updateUserProfile} from '../controllers/user.controller.js'
+import {followUnfollowUser, getSuggestedUsers, getUserProfile, updateUser} from '../controllers/user.controller.js'
 import { protectedRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/profile/:username',protectedRoute,getUserProfile);
 router.get('/suggestedUsers',protectedRoute,getSuggestedUsers);
 router.post('/follow/:id',protectedRoute,followUnfollowUser);
-router.post('/update',protectedRoute,updateUserProfile);
+router.post('/update',protectedRoute,updateUser);
 
 
 export default router
